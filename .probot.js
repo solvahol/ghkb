e735639c5633a3f2709793c78a4418ce2ba25830
Roger D. Winans <solvaholic@users.noreply.github.com>
2017-04-15 11:14:17 -0400

    Replace the issues.opened comment
    Use a simpler example from [the PRobot Configuration doc](https://github.com/probot/workflow/blob/master/docs/configuration.md)

--- .probot.js
+++ .probot.js
@@ -1,9 +1,4 @@
-on('issues.opened').comment(`
-  Hello @{{ sender.login }}. Thanks for inviting me to your project.
-  Read more about [all the things I can help you with][config]. I can't
-  wait to get started!
-
-  [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
-`);
-
-include("probot/workflow:docs/demo.js");
+// Auto-respond to new issues and pull requests
+on('issues.opened', 'pull_request.opened')
+  .comment('Thanks for your contribution! Expect a reply within 48 hours.')
+  .label('triage');
