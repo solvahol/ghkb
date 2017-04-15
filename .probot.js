dd114f0130032e6c03acdaab862ac40b214346ce
Roger D. Winans <solvaholic@users.noreply.github.com>
2017-04-15 10:45:03 -0400

    Invite PRobot to Solvahol
    For reals this time

---
+++ .probot.js
@@ -0,0 +1,9 @@
+on('issues.opened').comment(`
+  Hello @{{ sender.login }}. Thanks for inviting me to your project.
+  Read more about [all the things I can help you with][config]. I can't
+  wait to get started!
+
+  [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
+`);
+
+include("probot/workflow:docs/demo.js");
